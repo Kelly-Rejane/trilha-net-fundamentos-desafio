0679272a2e54d0bb14f461e1c98d3b51a4dbd715
Kelly Ferreira <kelly.estudos23@gmail.com>
2024-09-10 00:24:14 -0300

    Desafio finalizado

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -10,13 +10,13 @@ Console.WriteLine("Seja bem vindo ao sistema de estacionamento!\n" +
                   "Digite o preço inicial:");
 precoInicial = Convert.ToDecimal(Console.ReadLine());
 
-Console.WriteLine("Agora digite o preço por hora:");
+Console.WriteLine("Digite o preço por hora:");
 precoPorHora = Convert.ToDecimal(Console.ReadLine());
 
 // Instancia a classe Estacionamento, já com os valores obtidos anteriormente
 Estacionamento es = new Estacionamento(precoInicial, precoPorHora);
 
-string opcao = string.Empty;
+string opcao = string.Empty; //inicializa uma variável chamada com uma string vazia.
 bool exibirMenu = true;
 
 // Realiza o loop do menu
